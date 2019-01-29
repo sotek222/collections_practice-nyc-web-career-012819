841a964ca606b0f1b6a22674aaa3c059c16bac54
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-01-28 22:34:30 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
+require 'pry'
 
+def sort_array_asc(integers)
+  integers.sort
+end
+
+def sort_array_desc(integers)
+  integers.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by{|word| word.length}
+end
+
+def swap_elements(names)
+  names[1], names[2] = names[2], names[1]
+  names
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(names)
+  names.map do |name|
+    name[2] = '$'
+  end
+  names
+end
+
+def find_a(words)
+  a_words = words.select {|word| word.start_with?("a")}
+end
+
+def sum_array(numbers)
+  sum = 0
+  numbers.each do |number|
+    sum += number
+  end
+  sum
+end
+
+def add_s(words)
+  words.map do |word|
+    if word != words[1]
+      word << "s"
+    else
+      word
+    end
+  end
+end
